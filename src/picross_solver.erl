4d5c3c03ad8c3f840c71ac604b973d3b66207d44
Rafael Lorandi <coolparadox@gmail.com>
2020-06-07 01:05:18 -0300

    Refactor solver for gen_statem behaviour

--- src/picross_solver.erl
+++ src/picross_solver.erl
@@ -1,15 +1,175 @@
 -module(picross_solver).
--export([test/0,
-         start/5,
-         start_link/5,
-         set_solvers/2,
-         go/1,
-         terminate/1,
-         init/5,
-         str_to_map/1,
-         map_to_str/1]).
--record(state, { tag, id, length, fills, clue, manager, solvers=[], stalled=false }).
+-behaviour(gen_statem).
+-export([test/0]).
+-export([str_to_map/1, map_to_str/1]).
+-export([start/4, start_link/4, stop/1, prime/2, retire/1]).
+-export([callback_mode/0, init/1]).
+-export([priming/3, discovering/3, stalled/3, resting/3, retired/3]).
+-record(state, { id, length, fills, listener, solvers=[], clue=[] }).
 -define(StallTimeout, 100).
+
+% Tests
+
+test() ->
+
+    [fill, gap, unknown] = str_to_map("#.?"),
+    "#.?" = map_to_str([fill, gap, unknown]),
+
+    GetNextMessage = fun() ->
+        receive
+            Message -> Message
+        after 2 * ?StallTimeout ->
+                  error(timeout)
+        end
+    end,
+
+    % Solve a filled 1x1 puzzle.
+    ok = fun() ->
+
+        { ok, RowSolver } = start_link(1, 1, [1], self()),
+        { ok, ColSolver } = start_link(1, 1, [1], self()),
+
+        % This puzzle is immediately solved; no hint is required.
+        ok = prime(RowSolver, [ColSolver]),
+        { RowSolver, resting } = GetNextMessage(),
+        ok = prime(ColSolver, [RowSolver]),
+        { ColSolver, resting } = GetNextMessage(),
+
+        % Check solution.
+        [fill] = retire(RowSolver),
+        { RowSolver, retired } = GetNextMessage(),
+        [fill] = retire(ColSolver),
+        { ColSolver, retired } = GetNextMessage(),
+
+        % Release resources.
+        ok = stop(RowSolver),
+        ok = stop(ColSolver),
+
+        ok
+    end(),
+
+    % Same as above, but one solver receives a nonsense hint.
+    ok = fun() ->
+
+        { ok, RowSolver } = start_link(1, 1, [1], self()),
+        { ok, ColSolver } = start_link(1, 1, [1], self()),
+        ok = prime(RowSolver, [ColSolver]),
+        { RowSolver, resting } = GetNextMessage(),
+        ok = prime(ColSolver, [RowSolver]),
+        { ColSolver, resting } = GetNextMessage(),
+
+        % Check for acknowledgement of a bad hint.
+        ok = hint(RowSolver, 1, gap),
+        { RowSolver, badhint } = GetNextMessage(),
+
+        [fill] = retire(RowSolver),
+        { RowSolver, retired } = GetNextMessage(),
+        [fill] = retire(ColSolver),
+        { ColSolver, retired } = GetNextMessage(),
+        ok = stop(RowSolver),
+        ok = stop(ColSolver),
+
+        ok
+    end(),
+
+    % Solve a puzzle with a unique solution:
+    % ##
+    % .#
+    ok = fun() ->
+
+        { ok, RowSolver1 } = start_link(1, 2, [2], self()),
+        { ok, RowSolver2 } = start_link(2, 2, [1], self()),
+        { ok, ColSolver1 } = start_link(1, 2, [1], self()),
+        { ok, ColSolver2 } = start_link(2, 2, [2], self()),
+
+        ok = prime(RowSolver1, [ColSolver1, ColSolver2]),
+        { RowSolver1, resting } = GetNextMessage(),
+        ok = prime(RowSolver2, [ColSolver1, ColSolver2]),
+        { RowSolver2, discovering } = GetNextMessage(),
+        ok = prime(ColSolver1, [RowSolver1, RowSolver2]),
+        { ColSolver1, discovering } = GetNextMessage(),
+        { ColSolver1, resting } = GetNextMessage(),
+        { RowSolver2, resting } = GetNextMessage(),
+        ok = prime(ColSolver2, [RowSolver1, RowSolver2]),
+        { ColSolver2, resting } = GetNextMessage(),
+
+        [fill, fill] = retire(RowSolver1),
+        { RowSolver1, retired } = GetNextMessage(),
+        [gap, fill] = retire(RowSolver2),
+        { RowSolver2, retired } = GetNextMessage(),
+        [fill, gap] = retire(ColSolver1),
+        { ColSolver1, retired } = GetNextMessage(),
+        [fill, fill] = retire(ColSolver2),
+        { ColSolver2, retired } = GetNextMessage(),
+
+        ok = stop(RowSolver1),
+        ok = stop(RowSolver2),
+        ok = stop(ColSolver1),
+        ok = stop(ColSolver2),
+
+        ok
+
+    end(),
+
+    % Feed an ambiguous puzzle:
+    % .# | #.
+    % #. | .#
+    ok = fun() ->
+
+        { ok, RowSolver1 } = start_link(1, 2, [1], self()),
+        { ok, RowSolver2 } = start_link(2, 2, [1], self()),
+        { ok, ColSolver1 } = start_link(1, 2, [1], self()),
+        { ok, ColSolver2 } = start_link(2, 2, [1], self()),
+
+        ok = prime(RowSolver1, [ColSolver1, ColSolver2]),
+        { RowSolver1, discovering } = GetNextMessage(),
+        ok = prime(RowSolver2, [ColSolver1, ColSolver2]),
+        { RowSolver2, discovering } = GetNextMessage(),
+        ok = prime(ColSolver1, [RowSolver1, RowSolver2]),
+        { ColSolver1, discovering } = GetNextMessage(),
+        ok = prime(ColSolver2, [RowSolver1, RowSolver2]),
+        { ColSolver2, discovering } = GetNextMessage(),
+
+        % This puzzle leads to a dead end.
+        { RowSolver1, stalled } = GetNextMessage(),
+        { RowSolver2, stalled } = GetNextMessage(),
+        { ColSolver1, stalled } = GetNextMessage(),
+        { ColSolver2, stalled } = GetNextMessage(),
+
+        % Let's offer a hint to one of the solvers in order it reaches a solution.
+        % This should cascade hints to the remaining solvers leading to a possible solution to the puzzle.
+        ok = hint(RowSolver1, 1, fill),
+        { RowSolver1, discovering } = GetNextMessage(),
+        { RowSolver1, resting } = GetNextMessage(),
+        { ColSolver2, discovering } = GetNextMessage(),
+        { ColSolver2, resting } = GetNextMessage(),
+        { RowSolver2, discovering } = GetNextMessage(),
+        { RowSolver2, resting } = GetNextMessage(),
+        { ColSolver1, discovering } = GetNextMessage(),
+        { ColSolver1, resting } = GetNextMessage(),
+
+        [fill, gap] = retire(RowSolver1),
+        { RowSolver1, retired } = GetNextMessage(),
+        [gap, fill] = retire(RowSolver2),
+        { RowSolver2, retired } = GetNextMessage(),
+        [fill, gap] = retire(ColSolver1),
+        { ColSolver1, retired } = GetNextMessage(),
+        [gap, fill] = retire(ColSolver2),
+        { ColSolver2, retired } = GetNextMessage(),
+
+        ok = stop(RowSolver1),
+        ok = stop(RowSolver2),
+        ok = stop(ColSolver1),
+        ok = stop(ColSolver2),
+
+        ok
+
+    end(),
+
+    ok.
+
+
+% Module API
 
 str_to_map(Str) ->
     lists:map(
@@ -31,166 +191,117 @@ map_to_str(Map) ->
             end
         end, Map).
 
-test() ->
+start(Id, Length, Fills, Listener) ->
+    gen_statem:start(
+        ?MODULE,
+        #state{id=Id, length=Length, fills=Fills, listener=Listener},
+        []).
 
-    [fill, gap, unknown] = str_to_map("#.?"),
-    "#.?" = map_to_str([fill, gap, unknown]),
+start_link(Id, Length, Fills, Listener) ->
+    gen_statem:start_link(
+        ?MODULE,
+        #state{id=Id, length=Length, fills=Fills, listener=Listener},
+        []).
 
-    StartMessageForwarder = fun(Tag, Pid) ->
-        spawn_link(fun Forwarder() ->
-            receive
-                Message ->
-                    Pid ! { Tag, Message },
-                    Forwarder()
-                after 10 * ?StallTimeout -> ok
-            end
-        end)
-    end,
+prime(Solver, Solvers) ->
+    gen_statem:cast(Solver, { prime, Solvers }).
 
-    GetNextMessage = fun() ->
-        receive
-            Message -> Message
-        after 2 * ?StallTimeout ->
-                  error(timeout)
-        end
-    end,
+retire(Solver) ->
+    gen_statem:call(Solver, retire).
 
-    % Solve '1' in the first row of a puzzle with 1 column.
-    Solver1 = start_link(1, 1, [1], tag, self()),
-    set_solvers(Solver1, [StartMessageForwarder(1, self())]),
-    go(Solver1),
-    % This puzzle can be solved immediately.
-    { Solver1, done, [fill] } = GetNextMessage(),
-    % Also the solver of the first column should be hinted that its first position is filled.
-    { 1, { hint, 1, fill } } = GetNextMessage(),
-    % Verify termination.
-    terminate(Solver1),
-    { Solver1, terminated } = GetNextMessage(),
+stop(Solver) ->
+    gen_statem:stop(Solver).
 
-    % Same as above, but the solver receives a nonsense hint.
-    Solver1a = start_link(1, 1, [1], tag, self()),
-    set_solvers(Solver1a, [StartMessageForwarder(1, self())]),
-    go(Solver1a),
-    { Solver1a, done, [fill] } = GetNextMessage(),
-    { 1, { hint, 1, fill } } = GetNextMessage(),
-    % Simulate the solver of the column hinting an absurd information.
-    % The bad hint should be acknowledged.
-    Solver1a ! { hint, 1, gap },
-    { Solver1a, badhint } = GetNextMessage(),
-    { Solver1a, done, [fill] } = GetNextMessage(),
-    % Verify termination.
-    terminate(Solver1a),
-    { Solver1a, terminated } = GetNextMessage(),
 
-    % Solve '2' in the fifth row of a puzzle with 3 columns.
-    Solver2 = start_link(5, 3, [2], tag, self()),
-    set_solvers(Solver2, lists:map(fun(Id) -> StartMessageForwarder(Id, self()) end, [1, 2, 3])),
-    go(Solver2),
-    % The initial information is only enough to determine that the middle position is filled, ie: '?#?'
-    % Therefore the solver of the second column should be hinted that its fifth position is a fill.
-    { 2, { hint, 5, fill } } = GetNextMessage(),
-    % No further work is possible with the available information; check for stall detection.
-    { Solver2, stalled } = GetNextMessage(),
-    % Let's simulate receiving a hint from the solver of the first column.
-    Solver2 ! { hint, 1, gap },
-    % This is a useful information that leads to the solution '.##'
-    % and subsequently hinting the remaining solver of the third column.
-    { Solver2, working } = GetNextMessage(),
-    { Solver2, done, [gap, fill, fill] } = GetNextMessage(),
-    { 3, { hint, 5, fill } } = GetNextMessage(),
-    % Verify termination.
-    terminate(Solver2),
-    { Solver2, terminated } = GetNextMessage(),
+% Mandatory gen_statem callbacks
 
-    ok.
+callback_mode() -> [state_functions, state_enter].
 
-start(Id, Length, Fills, Tag, Manager) ->
-    spawn(?MODULE, init, [Id, Length, Fills, Tag, Manager]).
-
-start_link(Id, Length, Fills, Tag, Manager) ->
-    spawn_link(?MODULE, init, [Id, Length, Fills, Tag, Manager]).
-
-init(Id, Length, Fills, Tag, Manager) ->
-    case check_inputs(Fills, Length) of
-        false -> badarg;
-        true ->
-            solver(working, #state{tag=Tag, id=Id, length=Length, fills=Fills, clue=emerge_clue(map_combine(Fills, Length)), manager=Manager})
+init(S) ->
+    case check_inputs(S#state.fills, S#state.length) of
+        false -> { stop, invalid };
+        true -> { ok, priming, S }
     end.
 
-set_solvers(Solver, Solvers) ->
-    Solver ! { solvers, Solvers }.
 
-go(Solver) ->
-    Solver ! go.
+% State functions
 
-terminate(Solver) ->
-    Solver ! terminate.
+priming(enter, _, _) ->
+    keep_state_and_data;
+priming(cast, { hint, _, _ }, _) ->
+    { keep_state_and_data, postpone };
+priming(cast, {prime, Solvers}, S) ->
+    Clue = emerge_clue(map_combine(S#state.fills, S#state.length)),
+    hint_solvers(S#state.id, Solvers, Clue),
+    NS = S#state{solvers=Solvers, clue=Clue},
+    case lists:member(unknown, Clue) of
+        true ->
+            { next_state, discovering, NS };
+        false ->
+            { next_state, resting, NS }
+    end.
+
+discovering(enter, _, S) ->
+    tell(S, discovering),
+    { keep_state_and_data, ?StallTimeout };
+discovering(timeout, _, S) ->
+    { next_state, stalled, S };
+discovering(cast, { hint, Position, Hint }, S) ->
+    { HintCategory, HintedClue } = acknowledge_hint(S#state.clue, Position, Hint),
+    case HintCategory of
+        nonsense ->
+            tell(S, badhint),
+            keep_state_and_data;
+        known ->
+            keep_state_and_data;
+        useful ->
+            NewClue = emerge_clue(HintedClue, map_combine(S#state.fills, S#state.length)),
+            hint_solvers(S#state.id, S#state.solvers, emerge_hints(HintedClue, NewClue)),
+            NS = S#state{clue=NewClue},
+            case lists:member(unknown, NewClue) of
+                true ->
+                    { keep_state, NS };
+                false ->
+                    { next_state, resting, NS }
+            end
+    end.
+
+stalled(enter, _, S) ->
+    tell(S, stalled),
+    keep_state_and_data;
+stalled(cast, { hint, _, _ }, S) ->
+    { next_state, discovering, S, postpone }.
+
+resting(enter, _, S) ->
+    tell(S, resting),
+    keep_state_and_data;
+resting(cast, { hint, Position, Hint }, S) ->
+    { HintCategory, _ } = acknowledge_hint(S#state.clue, Position, Hint),
+    case HintCategory of
+        nonsense -> tell(S, badhint);
+        _ -> ok
+    end,
+    keep_state_and_data;
+resting({call, From}, retire, S) ->
+    { next_state, retired, S, { reply, From, S#state.clue } }.
+
+retired(enter, _, S) ->
+    tell(S, retired),
+    keep_state_and_data.
+
+
+% Support functions
+
+hint(Solver, Position, Hint) ->
+    gen_statem:cast(Solver, { hint, Position, Hint }).
+
+tell(S, Atom) ->
+    S#state.listener ! { self(), Atom }.
 
 check_inputs([], _) -> false;
 check_inputs(Fills, Max) ->
     is_list(Fills)
     andalso lists:all(fun(Val) -> Val > 0 andalso Val =< Max end, Fills).
-
-solver(working, S) ->
-    receive
-        { solvers, Solvers } ->
-            solver(working, S#state{solvers=Solvers, stalled=false});
-        go ->
-            hint_solvers(S#state.id, S#state.solvers, S#state.clue),
-            case lists:member(unknown, S#state.clue) of
-                true ->
-                    solver(working, S#state{stalled=false});
-                false ->
-                    solver(done, S#state{stalled=false})
-            end;
-        { hint, Position, Hint } ->
-            % io:format("~w: got hint: ~B ~w~n", [self(), Position, Hint]),
-            case S#state.stalled of
-                true -> S#state.manager ! { self(), working };
-                false -> ok
-            end,
-            { HintQuality, HintedClue } = acknowledge_hint(S#state.clue, Position, Hint),
-            case HintQuality of
-                nonsense ->
-                    S#state.manager ! { self(), badhint },
-                    solver(working, S#state{stalled=false});
-                known ->
-                    solver(working, S#state{stalled=false});
-                useful ->
-                    NewClue = emerge_clue(HintedClue, map_combine(S#state.fills, S#state.length)),
-                    hint_solvers(S#state.id, S#state.solvers, emerge_hints(HintedClue, NewClue)),
-                    case lists:member(unknown, NewClue) of
-                        true ->
-                            solver(working, S#state{clue=NewClue, stalled=false});
-                        false ->
-                            solver(done, S#state{clue=NewClue, stalled=false})
-                    end
-            end;
-        terminate ->
-            S#state.manager ! { self(), terminated };
-        Unexpected ->
-            error(unexpected, [Unexpected])
-    after case S#state.stalled of true -> infinity; false -> ?StallTimeout end ->
-        S#state.manager ! { self(), stalled },
-        solver(working, S#state{stalled=true})
-    end;
-solver(done, S) ->
-    S#state.manager ! { self(), done, S#state.clue },
-    receive
-        { hint, Position, Hint } ->
-            { HintQuality, _ } = acknowledge_hint(S#state.clue, Position, Hint),
-            case HintQuality of
-                nonsense ->
-                    S#state.manager ! { self(), badhint },
-                    solver(done, S);
-                known ->
-                    solver(done, S)
-            end;
-        terminate ->
-            S#state.manager ! { self(), terminated };
-        Unexpected ->
-            error(unexpected, [Unexpected])
-    end.
 
 % emerge_hints([fill,gap,unknown,unknown],[fill,gap,fill,gap]) -> [unknown,unknown,fill,gap]
 emerge_hints(OldHints, NewHints) ->
@@ -203,7 +314,7 @@ emerge_hints(OldHints, NewHints) ->
         end,
         [],
         lists:zip(OldHints, NewHints)
-    ).
+        ).
 
 acknowledge_hint(Clue, Position, Hint) ->
     case { lists:nth(Position, Clue), Hint } of
@@ -222,14 +333,10 @@ hint_solvers(_, [], []) -> ok;
 hint_solvers(Position, [_|Solvers], [unknown|Hints]) ->
     hint_solvers(Position, Solvers, Hints);
 hint_solvers(Position, [Solver|Solvers], [Hint|Hints]) ->
-    Solver ! { hint, Position, Hint },
+    hint(Solver, Position, Hint),
     hint_solvers(Position, Solvers, Hints).
 
 map_combine(Fills, Length) -> lists:map(fun picr_to_map/1, picr_combine(Fills, Length)).
-
-% Maps = lists:map(fun picross:picr_to_map/1, picross:picr_combine([8], 10)).
-% FirstClue = picross:emerge_clue(Maps).
-% NewClue = picross:emerge_clue(OldClue, Maps).
 
 emerge_clue([Map|Maps]) -> emerge_clue(lists:duplicate(length(Map), unknown), [Map|Maps]).
 
